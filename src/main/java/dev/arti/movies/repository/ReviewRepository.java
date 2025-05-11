246817b4895b249112adddff46d970adb9b0ced3
ArtiKumari92 <artiyadav574@gmail.com>
2025-05-11 22:13:56 +0530

    Movie Review backend

---
+++ src/main/java/dev/arti/movies/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.arti.movies.repository;
+
+import dev.arti.movies.model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
